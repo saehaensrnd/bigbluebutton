c2d96721cf83fe49a0a5d92804cb38fc14d5fb96
saehaensrnd <saehaensrnd@gmail.com>
2022-05-23 05:56:45 +0000

    add ments

--- bigbluebutton-html5/imports/utils/statuses.js
+++ bigbluebutton-html5/imports/utils/statuses.js
@@ -1,13 +1,21 @@
 export const EMOJI_STATUSES = {
   // name: icon
-    away: 'time',
+
+
     speakLouder: 'confused',
-    speakSofter : 'confused',
-    speakFaster: 'confused',
-    speakSlower : 'confused',
-    thumbsUp: 'thumbs_up',
-    thumbsDown: 'thumbs_down',
+    notHear : 'confused',
+    notCam : 'confused',
+    logoutNlogin : 'confused',
+    checkMic : 'confused',
+    teacherLogout : 'confused',
+    callParent : 'confused',
+    seePicture : 'confused',
+    repeat : 'confused',
+    sayAgain : 'confused',
+    thumbsUp : 'confused',
     whiteboard : 'confused',
+
+
 };
 
 export default { EMOJI_STATUSES };
--- bigbluebutton-html5/imports/utils/student_statuses.js
+++ bigbluebutton-html5/imports/utils/student_statuses.js
@@ -1,11 +1,27 @@
 export const STUDENT_EMOJI_STATUSES = {
     // name: icon
-    away: 'time',
+
+
+
+    //away: 'time',
     raiseHand: 'hand',
-    speakLouder: 'confused',
-    speakSofter : 'confused',
-    speakFaster: 'confused',
+    //speakLouder: 'confused',
+    //speakSofter : 'confused',
+    //speakFaster: 'confused',
     speakSlower : 'confused',
+    notKnow : 'confused',
+    sayAgain : 'confused',
+    notHear : 'confused',
+    notCam : 'confused',
+    notTextbook : 'confused',
+    notWhiteboard : 'confused',
+    notPen : 'confused',
+    usePen : 'confused'
+
+
+
+
+
   };
 
   export default { STUDENT_EMOJI_STATUSES };
